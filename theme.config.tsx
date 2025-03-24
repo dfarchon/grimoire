bcbde91b22ab126c661b725bb20bd6442f076e97
fromddy <ddy_mainland@163.com>
2025-03-24 14:59:57 +0800

    feat: enhance Grimoire SEO and update introduction content

--- theme.config.tsx
+++ theme.config.tsx
@@ -11,6 +11,29 @@ const config: DocsThemeConfig = {
     text: 'GPLv3 2025 © DFArchon',
   },
   primaryHue: 340,
+  useNextSeoProps() {
+    return {
+      titleTemplate: '%s - Grimoire',
+      defaultTitle: 'Grimoire',
+      description: 'Grimoire shapes the Onchain Reality Interaction Protocols, infused with the will of the DFArchon community.',
+      openGraph: {
+        type: 'website',
+        locale: 'en_US',
+        url: 'https://grimoire.dfmud.xyz/',
+        siteName: 'Grimoire',
+        title: 'Grimoire',
+        description: 'Grimoire shapes the Onchain Reality Interaction Protocols, infused with the will of the DFArchon community.',
+        images: [
+          {
+            url: '/og-image.png',
+            width: 1200,
+            height: 630,
+            alt: 'DFArchon Grimoire'
+          }
+        ]
+      }
+    }
+  }
 }
 
 export default config
